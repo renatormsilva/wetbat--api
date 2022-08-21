fc2955e988f92cc414bde1dd344f9ce2e9cfcce6
renatormsilva <renato.brtkm@gmail.com>
2022-08-21 18:31:21 -0400

    refactor: allow methods

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Private-Network", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   app.use(cors());
   next();
 });
